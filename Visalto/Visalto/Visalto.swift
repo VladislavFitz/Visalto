fc144784dcacdb94274363d96aa313b90b02e75f
Vladislav Fitc <vladislav.fitz@gmail.com>
2018-08-02 22:19:21 +0200

    Visalto singletone

---
+++ Visalto/Visalto/Visalto.swift
@@ -0,0 +1,19 @@
+//
+//  Visalto.swift
+//  Visalto
+//
+//  Created by Vladislav Fitc on 02/08/2018.
+//  Copyright © 2018 Vladislav Fitc. All rights reserved.
+//
+
+import Foundation
+
+class Visalto {
+    
+    static let shared = Visalto()
+    
+    private init() {
+        
+    }
+    
+}
