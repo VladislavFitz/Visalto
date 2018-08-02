b85369d1208b406bfb4b1f4aff8200381a86fbff
Vladislav Fitc <vladislav.fitz@gmail.com>
2018-08-02 22:17:56 +0200

    xcode project

---
+++ Visalto/Visalto/Visalto.h
@@ -0,0 +1,19 @@
+//
+//  Visalto.h
+//  Visalto
+//
+//  Created by Vladislav Fitc on 02/08/2018.
+//  Copyright © 2018 Vladislav Fitc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Visalto.
+FOUNDATION_EXPORT double VisaltoVersionNumber;
+
+//! Project version string for Visalto.
+FOUNDATION_EXPORT const unsigned char VisaltoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Visalto/PublicHeader.h>
+
+
